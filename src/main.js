d14814c3e35d6207dcd36e67388f858ded3ff122
YEndless <895315416@qq.com>
2018-10-24 09:25:16 +0800

    简书登录

--- src/main.js
+++ src/main.js
@@ -7,10 +7,15 @@ import bootstrap from 'bootstrap-vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import axios from 'axios'
 
 library.add(faCoffee)
 
+
+Vue.use(jquery)
+Vue.use(bootstrap)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.prototype.$http = axios
 
 Vue.use(jquery)
 Vue.use(bootstrap)
